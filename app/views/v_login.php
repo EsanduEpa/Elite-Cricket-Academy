a3c8ac8d25e62b6ef7efa3d70d8dd08acd3a9aa7
EsanduEpa <esanduepa0225.com>
2025-09-02 15:00:59 +0530

    redotest

--- app/views/v_login.php
+++ app/views/v_login.php
@@ -21,7 +21,7 @@
                     <h1 class="academy-title">Elite Cricket Academy</h1>
                     
                     <div class="login-box">
-                        <h2>LOGIN23</h2>
+                        <h2>LOGIN</h2>
                         
                         <form id="loginForm" class="login-form">
                             <div class="form-group">
