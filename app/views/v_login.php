6da60cb7e657c18187e88a3137e89ef7edb6c928
Venuki <venukiwickramarachchi@gmail.com>
2025-10-19 23:14:58 +0530

    Fix login page syntax error and update logout functionality

--- app/views/v_login.php
+++ app/views/v_login.php
@@ -46,7 +46,7 @@
                         
                         <div class="register-link">
                             <span>Don't have an account? </span>
-                            <a href="<?php echo URLROOT; ?>/register"">Register</a>
+                            <a href="<?php echo URLROOT; ?>/register">Register</a>
                         </div>
 
                         
